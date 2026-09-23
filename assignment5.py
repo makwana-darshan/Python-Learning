ee94a84b45b40ab62b690c888cde19a51e9fefb7
makwana-darshan <makwanadarshan6113@gmail.com>
2026-09-23 18:46:41 +0530

    String

---
+++ assignment5.py
@@ -0,0 +1,23 @@
+# 1
+email = "makwanadarshan901@gmail.com"
+
+print(email.split("@")[0])
+
+# 2
+sentence = "Java is good but Python is fun"
+
+print(sentence.count("is"))
+
+# 3
+word = "developer"
+
+print(word[::-1])
+
+# 4
+phone = "+91 1122556688"
+result = "Indian number" if phone.startswith("+91") else "Unknown"
+print(result)
+
+# 5
+full_name = "darshan makwana"
+print(full_name.title())
